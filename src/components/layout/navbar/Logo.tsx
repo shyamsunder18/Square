3febcbd726f51fbec17a450df5f5668474fdbcac
shyamsunder18 <167500462+shyamsunder18@users.noreply.github.com>
2025-04-12 00:44:28 +0530

    Update Logo.tsx

--- src/components/layout/navbar/Logo.tsx
+++ src/components/layout/navbar/Logo.tsx
@@ -1,15 +1,14 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import { ShoppingCart } from "lucide-react";
-
+import { SquareSquare } from 'lucide-react';
 export const Logo: React.FC = () => {
   return (
     <Link to="/" className="flex items-center">
       <div className="w-10 h-10 rounded bg-primary text-white flex items-center justify-center mr-2">
-        <ShoppingCart size={20} />
+        <SquareSquare size={20} />
       </div>
-      <span className="text-xl font-bold">E-Commerce</span>
+      <span className="text-xl font-bold">Square</span>
     </Link>
   );
 };

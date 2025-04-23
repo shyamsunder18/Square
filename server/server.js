9c22b54206154dbcbaa1e9a735f9dd900d420ea8
shyamsunder18 <167500462+shyamsunder18@users.noreply.github.com>
2025-04-24 00:34:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
